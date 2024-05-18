e303612c563a0ae04aa46345b8a2c8456b226a1e
Landon Williams <42020212+landwill@users.noreply.github.com>
2024-05-18 11:51:46 +1000

    wip: shape/rarity filters

--- src/ResultsScreen/ResultsScreen.tsx
+++ src/ResultsScreen/ResultsScreen.tsx
@@ -1,4 +1,5 @@
-import { Amulet, AmuletSummary, HtmlDumpInfo, Stat } from '../interfaces.ts'
+import React, { Reducer, useReducer } from 'react'
+import { Amulet, AmuletSummary, HtmlDumpInfo, Rarity, Shape, Stat } from '../interfaces.ts'
 import { AmuletGrid } from './AmuletGrid.tsx'
 
 function stringifyStats(stats: Stat[]): string {
@@ -21,7 +22,92 @@ function summarizeAmulets(amulets: Amulet[]): Map<string, AmuletSummary> {
   return amuletSummary
 }
 
+interface StatRequirement {
+  searchType: 'exact' | 'more'
+  value: number
+}
+
+interface StatSearch {
+  statName: string
+  statRequirement: StatRequirement
+}
+
+interface SearchDetails {
+  rarity: Rarity | null
+  shape: Shape | null
+  stats: StatSearch[] | null
+}
+
+interface SearchActionSetRarity {
+  action: 'setRarity'
+  rarity: Rarity | null
+}
+
+interface SearchActionSetShape {
+  action: 'setShape'
+  shape: Shape | null
+}
+
+type SearchAction = SearchActionSetRarity | SearchActionSetShape
+
+const searchReducer: Reducer<any, any> = (prev: SearchDetails, action: SearchAction): SearchDetails => {
+  console.log('Search action:', action)
+  if (action.action === 'setRarity') return { ...prev, rarity: action.rarity }
+  else if (action.action === 'setShape') return { ...prev, shape: action.shape }
+  console.log('Action not yet implemented', action)
+  return { ...prev }
+}
+
+const SearchPanel = ({ searchDispatcher }: { searchDispatcher: React.Dispatch<SearchAction> }) => {
+  // const [statName, setStatName] = useState('')
+  return <div>
+    <div onChange={e => {
+      const selectedRarity = e.target.value
+      searchDispatcher({ action: 'setRarity', rarity: selectedRarity === '' ? null : selectedRarity })
+    }}>
+      {/*<textarea defaultValue='' onChange={e => {searchDispatcher({ setName: e.target.value })}}/>*/}
+      <input type='radio' id='rarityAny' name='rarity' value='' />
+      <label htmlFor='rarityCommon'>Any</label>
+      <input type='radio' id='rarityLegendary' name='rarity' value={Rarity.Legendary} />
+      <label htmlFor='rarityLegendary'>Legendary</label>
+      <input type='radio' id='rarityRare' name='rarity' value={Rarity.Rare} />
+      <label htmlFor='rarityRare'>Rare</label>
+      <input type='radio' id='rarityCommon' name='rarity' value={Rarity.Common} />
+      <label htmlFor='rarityCommon'>Common</label>
+    </div>
+    <div onChange={e => {
+      const selectedShape = e.target.value
+      searchDispatcher({ action: 'setShape', shape: selectedShape === '' ? null : selectedShape })
+    }}>
+      {/*<textarea defaultValue='' onChange={e => {searchDispatcher({ setName: e.target.value })}}/>*/}
+      <input type='radio' id='shapeAny' name='shape' value='' />
+      <label htmlFor='shapeAny'>Any</label>
+      <input type='radio' id='shapeDiamond' name='shape' value='Diamond' />
+      <label htmlFor='shapeDiamond'>Diamond</label>
+      <input type='radio' id='shapeSquare' name='shape' value='Square' />
+      <label htmlFor='shapeSquare'>Square</label>
+      <input type='radio' id='shapeCircle' name='shape' value='Circle' />
+      <label htmlFor='shapeCircle'>Circle</label>
+    </div>
+  </div>
+}
+
+const amuletSatisfiesFilter = (amulet: AmuletSummary, searchDetails: SearchDetails): boolean => {
+  if (searchDetails.rarity && amulet.rarity != searchDetails.rarity) return false
+  if (searchDetails.shape && amulet.shape != searchDetails.shape) {
+    console.log(searchDetails.shape, amulet.shape)
+    return false
+  }
+  return true
+}
+
 export default function ResultsScreen({ inventoryHtml }: { inventoryHtml: HtmlDumpInfo[] | null }) {
+  const [searchDetails, dispatchSearch] = useReducer<(prev: SearchDetails, action: SearchAction) => SearchDetails>(searchReducer, {
+    rarity: null,
+    shape: null,
+    stats: []
+  })
+
   if (inventoryHtml == null || inventoryHtml.length === 0) return <div>No inventory data found.</div>
 
   const amulets = inventoryHtml.filter(dumpInfo => !dumpInfo.deleted).map(dumpInfo => dumpInfo.amulets)
@@ -29,8 +115,13 @@ export default function ResultsScreen({ inventoryHtml }: { inventoryHtml: HtmlDu
     .flatMap(amuletSummary => Array.from(amuletSummary.values()))
   const amuletTuples = [...summarizeAmulets(amulets)]
 
-  return <div style={{ display: 'flex', flexDirection: 'row', maxWidth: '1000px', flexWrap: 'wrap' }}>
-    <AmuletGrid amuletTuples={amuletTuples} />
-  </div>
+  console.log('Search details:', searchDetails)
+
+  return <>
+    <SearchPanel searchDispatcher={dispatchSearch} />
+    <div style={{ display: 'flex', flexDirection: 'row', maxWidth: '1000px', flexWrap: 'wrap' }}>
+      <AmuletGrid amuletTuples={amuletTuples.filter(([_, amulet]) => amuletSatisfiesFilter(amulet, searchDetails))} />
+    </div>
+  </>
 }
 

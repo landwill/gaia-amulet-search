d3047611c3bb4fd202b37fc95b3d8c78b51813c0
Landon Williams <42020212+landwill@users.noreply.github.com>
2024-05-18 21:20:11 +1000

    style: enhanced cards for search results

--- src/ResultsScreen/AmuletGrid.tsx
+++ src/ResultsScreen/AmuletGrid.tsx
@@ -1,3 +1,4 @@
+import { Card, Text } from '@mantine/core'
 import { AmuletSummary, AmuletTuple, Rarity } from '../interfaces.ts'
 import { AmuletImage } from './AmuletImage.tsx'
 import { stringifyStat, stringifyStats } from './utils.ts'
@@ -19,20 +20,34 @@ export const AmuletGrid = ({ amuletTuples }: { amuletTuples: [string, AmuletSumm
       amuletNameParts.push('Amulet')
       const amuletName = amuletNameParts.join(' ')
 
-      return <div key={`${amuletName}_${stringifyStats(amulet.stats)}`} style={{ border: '1px solid black', padding: '12px', margin: '6px', borderRadius: '12px', borderColor: 'lightgrey', width: '180px' }}>
-        <AmuletImage rarity={amulet.rarity} shape={amulet.shape} /><br />
-        <span style={{ fontWeight: 'bold' }}>{amuletName}</span><br />
-        <div style={{ marginTop: 6, marginBottom: 6 }}>
-          {
-            amulet.stats.length
-              ? amulet.stats.map(stat => <span style={{ display: 'inline-block' }}
-                                               key={stringifyStat(stat)}>{`+${String(stat.bonus)}% ${stat.statName}`}</span>)
-              : 'Experience vs [...]'
-          }</div>
-        {`Count: ${String(amulet.locations.length)}`}<br />
+      const stats = amulet.stats.length
+        ? amulet.stats.map(stat =>
+          <div key={stringifyStat(stat)}>
+            <Text size='xs' c='dimmed'>{stat.statName}</Text>
+            <Text fw={500} size='sm'>{`+${String(stat.bonus)}%`}</Text>
+          </div>)
+        : <Text size='xs'>Experience vs ...</Text>
+
+      return <Card withBorder key={`${amuletName}_${stringifyStats(amulet.stats)}`} style={{ margin: '6px', width: '180px' }}>
+        <Card.Section pt='md'>
+          <AmuletImage rarity={amulet.rarity} shape={amulet.shape} />
+        </Card.Section>
+        <Card.Section pl='md' pr='md'>
+          <Text style={{ fontSize: 'medium', fontWeight: 700 }}>{amuletName}</Text>
+        </Card.Section>
+        <Card.Section mb='auto'>
+          <div style={{
+            marginTop: 6,
+            paddingTop: 6,
+            borderTop: '1px solid light-dark(var(--mantine-color-gray-2), var(--mantine-color-dark-5))'
+          }}>
+            {stats}
+          </div>
+        </Card.Section>
+        <Text size='sm' c='dimmed' mt='sm'>Quantity: {String(amulet.locations.length)}</Text>
         {/*{amulet.locations.slice(0, 5).map(amuletLocation => <ul style={{ marginTop: 6, marginBottom: 6 }} key={amuletLocation.id} onClick={() => {
             navigator.clipboard.writeText(amuletLocation.id).catch((r: unknown) => {console.error(r)})
           }}>...{amuletLocation.id.substring(amuletLocation.id.length - 10)} (Page {amuletLocation.page})</ul>)}*/}
-      </div>
+      </Card>
     })
 }
--- src/ResultsScreen/AmuletImage.tsx
+++ src/ResultsScreen/AmuletImage.tsx
@@ -1,6 +1,8 @@
 import { Rarity, Shape } from '../interfaces.ts'
 import { amuletImageMap } from './utils.ts'
 
+const SIZE = 52
+
 export const AmuletImage = ({ rarity, shape }: { rarity: Rarity, shape: Shape }) => {
-  return <img style={{ display: 'inline' }} src={amuletImageMap[shape][rarity]} alt={`${Rarity[rarity]} ${shape} amulet`} />
+  return <img style={{ display: 'inline' }} src={amuletImageMap[shape][rarity]} alt={`${Rarity[rarity]} ${shape} amulet`} height={SIZE} width={SIZE} />
 }
